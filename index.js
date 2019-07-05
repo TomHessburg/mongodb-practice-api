3878189e1dbb138a0ed8245191e279d1ea99cc81
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-05 19:19:11 -0400

    added some virtuals, example indexing, and middleware hooks to the user schema for practice

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const { json, urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
 
+const User = require("./schemas/UserSchema.js");
+
 // server instantiation and middleware
 const server = express();
 server.use(cors());
@@ -16,6 +18,9 @@ server.use(morgan("dev"));
 
 // connect to db and listen
 const connect = () => {
+  // the connect method returns a promise,
+  // also must make sure to pass "useNewUrlParser: true" to get
+  // rid of that deprication warning.
   return mongoose.connect("mongodb://localhost:27017/referencedb", {
     useNewUrlParser: true
   });
@@ -24,8 +29,13 @@ const connect = () => {
 const port = process.env.PORT || 5000;
 connect()
   .then(async connection => {
+    const users = await User.find({}).exec();
     server.listen(port, () => {
-      console.log(`Server listening on ${port}`);
+      console.log(
+        `Server listening on ${port}, and there are currently ${
+          users.length
+        } users in the database!`
+      );
     });
   })
   .catch(err => {
---
+++ schemas/UserSchema.js
@@ -0,0 +1,72 @@
+// pull in mongoose
+const mongoose = require("mongoose");
+
+// set up how you want the schema to look! You can include timestamps simply by adding
+// the option as I did below.
+const user = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+      // "unique" will index a value in the database, giving it a O(1) lookup time
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 40
+    },
+    info: {
+      phone: {
+        type: Number
+      },
+      location: {
+        city: String,
+        state: String,
+        zip: Number,
+        address: String
+      }
+    }
+  },
+  { timestamps: true }
+);
+
+// indexing
+
+// a post could only have 1 user with the same username in this case
+// this is something that I could definitely dig futher into.
+// good indexing will dramatically improve performance,
+// and I didnt really get a deep enough dive on that!
+/*
+user.index({
+    username: 1,
+    post: 1
+},{ unique: true })
+*/
+
+// middleware / hooks / virtuals
+// middleware and virtuals must be function syntax, not arrow functions for 'this' scope
+user.pre("findById", function() {
+  console.log("Im a hook that runs before finding a user by ID");
+});
+user.post("save", function() {
+  console.log("Im a hook that runs after a user was saved");
+});
+user.post("find", function(doc, next) {
+  setTimeout(() => {
+    console.log("Im a fake asynchronous call that runs post finding a user.");
+    next();
+    // call next same way is normal express middleware. doc will also give reference
+    // to the document to do stuff with if need be
+  }, 200);
+});
+// virtuals wont show up up on the objects themselves, but will respond when called upon
+user.virtual("fullAddress").get(function() {
+  return `${this.address}, ${this.city} ${this.state} ${this.zip}`;
+});
+
+// Add the model and name it. Names should be singular!
+const User = mongoose.model("user", user);
+
+module.exports = User;
